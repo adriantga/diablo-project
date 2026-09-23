4e716ed5085d01c5c0e80575c9ffc1818042f791
Adrian Bagherpour <adrian.bagherpour@studerande.thegameassembly.se>
2026-09-23 16:11:09 +0200

    Added door and room class(es). TODO: Implement actual logic.

---
+++ Diablo/Structs.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+struct Player
+{
+    // This struct will take care of things like
+    // 1. Rooms traversed
+    // 2. Enemies killed(for scaling difficulty)
+    // More to come, possibly
+    // -----------------------------------------------
+    
+    int roomsCleared = 0;
+    int enemiesKilled = 0;
+};
+
+struct GameData
+{
+    Player player;
+};
