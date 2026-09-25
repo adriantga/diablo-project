6c747467fd79f8190d9c6def9bb50e05e3cb0139
Adrian Bagherpour <adrian.bagherpour@studerande.thegameassembly.se>
2026-09-25 10:00:30 +0200

    SADSDAASDASD

---
+++ Diablo/Utilities.cpp
@@ -0,0 +1,18 @@
+﻿#include "Utilities.h"
+
+#include <iostream>
+#include <random>
+
+static std::random_device globalRnd;
+static std::mt19937 globalGen(globalRnd());
+
+int GetRandomNumber(int aMin, int aMax)
+{
+    std::uniform_int_distribution<> distributor(aMin, aMax);
+    return distributor(globalGen);
+}
+
+void WriteLine(const char* aText, bool aNewLine)
+{
+    std::cout << aText << (aNewLine ? '\n' : '\0');
+}
---
+++ Diablo/Utilities.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+int GetRandomNumber(int aMin, int aMax);
+void WriteLine(const char* aText, bool aNewLine = true);
